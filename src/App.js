9bf10aa94665c1c8de9094ec0643d3922d87b495
MoRahh <moodyaburahma@gmail.com>
2022-08-07 13:35:50 +0300

    changed homepage url

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Sidebar />
       <Layout />
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/react-portfolio" element={<Layout />} />
         <Route index element={<Home />} />
         <Route path="about" index element={<About />} />
         <Route path="contact" index element={<Contact />} />
